67eecef6c2c39d0180f88c12e45b87955948f23f
Alex J Burke <alexjeffburke@gmail.com>
2019-10-08 18:56:09 +0200

    Add additional test cases for locations.

--- test/LocationEntry.spec.js
+++ test/LocationEntry.spec.js
@@ -3,6 +3,25 @@ const expect = require("unexpected");
 const LocationEntry = require("../lib/LocationEntry");
 
 describe("LocationEntry", () => {
+  it("should return coordinates correctly", () => {
+    const location = new LocationEntry({
+      long: -122,
+      lat: 60
+    });
+
+    expect(location.coordinates, "to equal", [-122, 60]);
+  });
+
+  it("should return cached coordinates after first read", () => {
+    const location = new LocationEntry({
+      long: -122,
+      lat: 60
+    });
+    const expectedCoordinates = location.coordinates;
+
+    expect(location.coordinates, "to be", expectedCoordinates);
+  });
+
   it("should exclude internals when converted to JSON", () => {
     const location = new LocationEntry({
       long: -122,
