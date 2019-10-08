cf8c2e3abe3850b01e71e0a108acf2d1c4003aae
Alex J Burke <alexjeffburke@gmail.com>
2019-10-08 19:02:01 +0200

    Add a test for an error being returned on nearest location failure.

--- test/handlers/intersection.spec.js
+++ test/handlers/intersection.spec.js
@@ -16,7 +16,7 @@ describe("intersection", () => {
     intersector = createMockIntersector();
   });
 
-  it("should respond", () => {
+  it("should respond with the nearest location result", () => {
     intersector.findNearestLocation.returns({ bob: 1, alice: 0 });
 
     return expect(intersectionHandler({ intersector }), "to yield exchange", {
@@ -27,6 +27,17 @@ describe("intersection", () => {
           "Content-Type": /^application\/json/
         },
         body: { bob: 1, alice: 0 }
+      }
+    });
+  });
+
+  it("should error if finding the nearest location fails", () => {
+    intersector.findNearestLocation.throws(new Error("fail"));
+
+    return expect(intersectionHandler({ intersector }), "to yield exchange", {
+      request: "/?long=0&lat=0",
+      response: {
+        errorPassedToNext: new Error("fail")
       }
     });
   });
