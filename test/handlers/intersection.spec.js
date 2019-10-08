fc9cfdb5acb48c2067ddefd55b2ccaf6e229d89d
Alex J Burke <alexjeffburke@gmail.com>
2019-10-08 15:05:17 +0200

    Wire the calculation code into the intersection handler.

--- test/handlers/intersection.spec.js
+++ test/handlers/intersection.spec.js
@@ -1,18 +1,32 @@
 const expect = require("unexpected")
   .clone()
   .use(require("unexpected-express"));
+const sinon = require("sinon");
 
 const intersectionHandler = require("../../lib/handlers/intersection");
 
+const createMockIntersector = () => ({
+  findNearestLocation: sinon.stub().named("findNearestLocation")
+});
+
 describe("intersection", () => {
+  let intersector;
+
+  beforeEach(() => {
+    intersector = createMockIntersector();
+  });
+
   it("should respond", () => {
-    return expect(intersectionHandler({}), "to yield exchange", {
+    intersector.findNearestLocation.returns({ bob: 1, alice: 0 });
+
+    return expect(intersectionHandler({ intersector }), "to yield exchange", {
       request: "/",
       response: {
         statusCode: 200,
         headers: {
           "Content-Type": /^application\/json/
-        }
+        },
+        body: { bob: 1, alice: 0 }
       }
     });
   });
