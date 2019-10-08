9497a756b2e0463aa17fc735017af852e2966861
Alex J Burke <alexjeffburke@gmail.com>
2019-10-08 18:45:12 +0200

    Alter loadData to check a file was supplied and test error cases.

--- test/loadData.spec.js
+++ test/loadData.spec.js
@@ -5,7 +5,31 @@ const loadData = require("../lib/loadData");
 const LocationEntry = require("../lib/LocationEntry");
 
 describe("loadData", () => {
-  it("should return LocatinEntry objects", () => {
+  it("should throw when passed a bogus path", () => {
+    const dirPath = path.join(__dirname, "..", "testdat");
+
+    expect(
+      () => {
+        loadData(dirPath);
+      },
+      "to throw",
+      `unable to load data from: ${dirPath}`
+    );
+  });
+
+  it("should throw when passed a directory", () => {
+    const dirPath = path.join(__dirname, "..", "testdata");
+
+    expect(
+      () => {
+        loadData(dirPath);
+      },
+      "to throw",
+      `unable to load data from: ${dirPath}`
+    );
+  });
+
+  it("should return LocationEntry objects", () => {
     const dataFile = path.join(__dirname, "..", "testdata", "example.csv");
 
     expect(
